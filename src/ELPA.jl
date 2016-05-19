c0e74e9970937893bc2be37da06be631ad47c130
afternone <afternone@163.com>
2016-05-19 20:11:24 +0800

    fix typo

--- src/ELPA.jl
+++ src/ELPA.jl
@@ -3,7 +3,7 @@ module ELPA
 typealias  Graph{V,W} Dict{V, Dict{V, W}}
 
 export addnode!, deletenode!, addedge!, deleteedge!, ne,
-    lpa_addnode!, lpa_deletenode!, lpa_addedge!, lpa_deleteedge!, loadgml, savegmal, modularity, Graph
+    lpa_addnode!, lpa_deletenode!, lpa_addedge!, lpa_deleteedge!, loadgml, savegml, modularity, Graph
 # package code goes here
 include("graph.jl")
 include("evolving.jl")
