4ec41184e9c04a2471e43a56a801bdca7d25b90a
afternone <afternone@163.com>
2016-05-25 14:10:54 +0800

    add modularity

--- src/elpa1.jl
+++ src/elpa1.jl
@@ -413,3 +413,37 @@ function graphdiff(graph1, graph2)
     end
     new_edges
 end
+
+function modularity(g, c)
+	Q = 0.
+	m = 2*ne(g)
+	m == 0 && return 0.
+	s1 = 0
+	s2 = 0
+	for u in nodes(g)
+        for v in neighbors(g,u)
+			c[u] != c[v] && continue
+			s1 += 1
+			s2 += length(g[u])*length(g[v])
+		end
+	end
+	Q = s1/m - s2/m^2
+	return Q
+end
+
+function modularity1(g, c)
+	Q = 0.
+	m = 2*ne(g)
+	m == 0 && return 0.
+	s1 = 0
+	s2 = 0
+	for u in keys(g)
+		for v in keys(g)
+			c[u] != c[v] && continue
+			s1 += in(v, g[u]) ? 1 : 0
+			s2 += length(g[u])*length(g[v])
+		end
+	end
+	Q = s1/m - s2/m^2
+	return Q
+end
